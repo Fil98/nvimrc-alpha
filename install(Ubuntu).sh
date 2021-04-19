1e4d2822bc4105278b0a211192fb76e77fd9f4c8
Fil_98 <45718052+Fil98@users.noreply.github.com>
2021-04-19 21:14:32 +0300

    Install in Ubuntu
    Nvim+plugins in ubuntu

---
+++ install(Ubuntu).sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+#install
+  
+  sudo apt-get install curl git python yarn nodejs xclip neovim ruby python3-pip exuberant-ctags 
+  sudo pip3 install neovim
+
+#create folders
+  
+  mkdir -p ~/.config/nvim/autoload
+  mkdir -p ~/.config/nvim/plugged
+  mkdir -p ~/.config/nvim/colors
+
+#install plug
+  
+  curl -fLo ~/.config/nvim/autoload/plug.vim \
+  https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+
+#install config
+  
+  cp * ~/.config/nvim/
+  cd ~
+  nvim -c PlugUpgrade -c PlugInstall -c PlugUpdate -c q -c q
+  nvim -c UpdateRemotePlugins -c q -c q
+
+  echo done!
